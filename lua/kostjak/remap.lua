41aeeb20ecebfb1681fd9aec1ff5925a208c847d
Jakub Kostiw <jakub.kostiw@dynamic-precision.pl>
2025-07-21 08:13:33 +0200

    Gemini and diagnostic remap

--- lua/kostjak/remap.lua
+++ lua/kostjak/remap.lua
@@ -54,3 +54,5 @@ vim.keymap.set('n','gr','<cmd>lua vim.lsp.buf.references()<CR>')
 vim.keymap.set('n','gs','<cmd>lua vim.lsp.buf.signature_help()<CR>')
 vim.keymap.set('n','gi','<cmd>lua vim.lsp.buf.implementation()<CR>')
 vim.keymap.set('n','gt','<cmd>lua vim.lsp.buf.type_definition()<CR>')
+
+vim.keymap.set('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>')
